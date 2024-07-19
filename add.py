e517fef2afc9687e2263efc496e83e82a62bc7e9
rweathers89 <rachelb.weathers@gmail.com>
2024-07-18 21:22:31 -0700

    first commit

---
+++ add.py
@@ -0,0 +1,11 @@
+# prompt the user to enter the first number
+a = int(input("Enter a number: "))
+
+#prompt the user to enter another number
+b = int(input("Enter another number: "))
+
+#add the number and store the result in variable c
+c = a + b
+
+#display the value of c
+print("The sume of", a, "and", b, "is: ", c)
